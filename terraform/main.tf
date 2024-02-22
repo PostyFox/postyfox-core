e457945685f51fb211f283a1dd20563fb390bdb3
Andy Neillans <andy@neillans.co.uk>
2024-02-22 21:10:34 +0000

    Removed subscription from the main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,4 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  # If you are deploying this onto your own tenant, you will want to change this
-  subscription_id = "b568099c-fb2f-4e51-946f-d9e40f80e73b"
 }
