f9c0a36ece279a8e5cf94503752a709cae803cd9
Andy Neillans <andy@neillans.co.uk>
2024-03-30 08:07:33 +0000

    Flatten AppInsight to single instance, add auth to nodejs function app

--- terraform/nodejs_function_app.tf
+++ terraform/nodejs_function_app.tf
@@ -11,6 +11,11 @@ resource "azurerm_linux_function_app" "nodejs_func_app" {
   storage_uses_managed_identity = true
   service_plan_id               = azurerm_service_plan.linux_func_service_plan.id
 
+  app_settings = {
+    "ConfigTable" = azurerm_storage_account.data_storage.primary_table_endpoint
+    "AAD_B2C_PROVIDER_AUTHENTICATION_SECRET" = "@Microsoft.KeyVault(VaultName=${local.appname}-kv${local.hyphen-env};SecretName=clientsecret)"
+  }
+
   identity {
     type = "SystemAssigned"
   }
@@ -20,7 +25,41 @@ resource "azurerm_linux_function_app" "nodejs_func_app" {
       node_version = 18
     }
 
-    application_insights_key = azurerm_application_insights.application_insights-node.instrumentation_key
+    application_insights_connection_string = azurerm_application_insights.application_insights.connection_string
+  }
+
+  auth_settings_v2 {
+    auth_enabled             = true
+    forward_proxy_convention = "NoProxy"
+    http_route_api_prefix    = "/.auth"
+    require_authentication   = true
+    require_https            = true
+    runtime_version          = "~1"
+    unauthenticated_action   = "Return401"
+    default_provider         = "AAD_B2C"
+
+    custom_oidc_v2 {
+      name = "AAD_B2C"
+      client_id = var.func_app_registered_client_id
+      openid_configuration_endpoint = var.openid_configuration_endpoint
+    }
+
+    login {
+      cookie_expiration_convention      = "FixedTime"
+      cookie_expiration_time            = "08:00:00"
+      logout_endpoint                   = "/.auth/logout"
+      nonce_expiration_time             = "00:05:00"
+      preserve_url_fragments_for_logins = false
+      token_refresh_extension_time      = 72
+      token_store_enabled               = true
+      validate_nonce                    = true
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [ 
+      app_settings["WEBSITE_ENABLE_SYNC_UPDATE_SITE"]
+    ]
   }
 }
 
