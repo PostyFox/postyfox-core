79f1b98e385d502fede3364f499e60dae85cee8a
Andy Neillans <andy@neillans.co.uk>
2024-04-10 18:20:53 +0100

    Super simplification of the telegram state storage

--- PostyFox-NetCore/Integrations/TelegramState.cs
+++ PostyFox-NetCore/Integrations/TelegramState.cs
@@ -1,23 +1,13 @@
-﻿using Azure.Data.Tables;
-using Azure.Storage.Blobs;
-using Microsoft.Extensions.Azure;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Azure.Storage.Blobs;
 
 namespace PostyFox_NetCore.Integrations
 {
-    class TelegramStore : Stream
+    class TelegramStore : MemoryStream
     {
         private readonly string _sessionName;
         private BlobServiceClient _blobServiceClient;
         private BlobContainerClient _containerClient;
         private BlobClient _blobClient;
-        private byte[] _data;
-        private int _dataLen;
 
         public TelegramStore(string sessionName, BlobServiceClient blobServiceClient)
         {
@@ -39,31 +29,14 @@ namespace PostyFox_NetCore.Integrations
             Flush();
         }
 
-        public override int Read(byte[] buffer, int offset, int count)
-        {
-            Array.Copy(_data, 0, buffer, offset, count);
-            return count;
-        }
-
-        public override void Write(byte[] buffer, int offset, int count) // Write call and buffer modifications are done within a lock()
-        {
-            _data = buffer; _dataLen = count;
-        }
-
-        public override long Length => _dataLen;
-        public override long Position { get => 0; set { } }
-        public override bool CanSeek => false;
-        public override bool CanRead => true;
-        public override bool CanWrite => true;
-        public override long Seek(long offset, SeekOrigin origin) => 0;
-        public override void SetLength(long value) { }
         public override void Flush() 
         {
-            if (_data != null && _dataLen > 0)
+            if (this.Length > 0)
             {
                 // Write the data back to Storage Account
                 _blobClient.Upload(this, true);
             }
         }
+
     }
 }
