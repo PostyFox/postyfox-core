e8fe1400df892dee7c17c4aad01f54ec2744171a
Andy Neillans <andy@neillans.co.uk>
2024-04-11 10:38:35 +0100

    Remove BlueSky from .NET code; Adding template Typescript / Node project; Adding service terraform

---
+++ terraform/service_templates.tf
@@ -0,0 +1,42 @@
+resource "azurerm_storage_table_entity" "telegram-template" {
+  storage_table_id = azurerm_storage_table.data_storage.id
+
+  partition_key = "Service"
+  row_key       = "Telegram"
+
+  entity = {
+    ServiceName   = "Telegram"
+    IsEnabled     = true
+    ServiceID     = "Telegram"
+    Configuration = "{\"PhoneNumber\":\"\"}"
+  }
+}
+
+resource "azurerm_storage_table_entity" "discordwh-template" {
+  storage_table_id = azurerm_storage_table.data_storage.id
+
+  partition_key = "Service"
+  row_key       = "DiscordWH"
+
+  entity = {
+    ServiceName   = "DiscordWH"
+    IsEnabled     = true
+    ServiceID     = "DiscordWH"
+    Configuration = "{\"Webhook\":\"\"}"
+  }
+}
+
+resource "azurerm_storage_table_entity" "bluesky-template" {
+  storage_table_id = azurerm_storage_table.data_storage.id
+
+  partition_key = "Service"
+  row_key       = "BlueSky"
+
+  entity = {
+    ServiceName         = "BlueSky"
+    IsEnabled           = true
+    ServiceID           = "BlueSky"
+    Configuration       = "{\"Handle\":\"\"}"
+    SecureConfiguration = "{\"AppPassword\":\"\"}"
+  }
+}
