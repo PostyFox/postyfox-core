1f2b4d3e242fdd435205d124d07380ebe4fc85a6
Andy Neillans <andy@neillans.co.uk>
2024-04-11 14:30:59 +0100

    WIP

--- PostyFox-TypeScript/BlueSky/isAuthenticated.ts
+++ PostyFox-TypeScript/BlueSky/isAuthenticated.ts
@@ -1,8 +1,36 @@
 import { app, HttpResponseInit, HttpRequest, InvocationContext } from "@azure/functions"
 import { BskyAgent } from "@atproto/api";
+import { SecretClient } from "@azure/keyvault-secrets";
+import { TableClient } from "@azure/data-tables";
+import { DefaultAzureCredential } from "@azure/identity";
+const { getUserId, validateAuth } = require("../Helpers/index");
 
 async function BlueSky_IsAuthenticated(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
+    if (validateAuth(request)) {
+        const credential = new DefaultAzureCredential();
+
+        // Service config for the user is held in ConfigTable
+        const configTableName = process.env["ConfigTable"];
+        if(!configTableName) throw new Error("ConfigTable is empty");
+
+        const client = new TableClient(configTableName, "ConfigTable", credential);
+        await client.createTable(); // Ensure it exists
+
+        const entity = await client.getEntity(getUserId(request), "BlueSky");
+
+        // The BlueSky identity for a user is made up of a SecureConfiguration component which is stored in KeyVault (their App Password)
+        // So we need to pull it now
+
+        
+
+        const keyVaultName = process.env["SecretStore"];
+        if(!keyVaultName) throw new Error("SecretStore is empty");
+
+    }
+
     return { body: "" };
+
+
 }
 
 app.http('BlueSky_IsAuthenticated', {
---
+++ PostyFox-TypeScript/Helpers/index.ts
@@ -0,0 +1,25 @@
+import { HttpRequest } from "@azure/functions"
+
+const AUTH_HEADER = "X-MS-CLIENT-PRINCIPAL";
+const AUTH_HEADER_ID = "X-MS-CLIENT-PRINCIPAL-ID";
+
+// Identifies if the user has an active session or not
+exports.validateAuth = function(request: HttpRequest): boolean {
+    const isDevMode = process.env["PostyFoxDevMode"];
+    if (!isDevMode) {
+        return request.headers.has(AUTH_HEADER);
+    } else {
+        return true;
+    }
+};
+
+
+// Gets the authenticated UserID
+exports.getUserId = function(request: HttpRequest): string {
+    const isDevMode = process.env["PostyFoxDevMode"];
+    if (!isDevMode) {
+        return request.headers.get(AUTH_HEADER_ID);
+    } else {
+        return process.env["PostyFoxUserID"];
+    }
+};
