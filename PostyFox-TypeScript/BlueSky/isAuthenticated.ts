692e38f81b7056f898f3d3309f3db702d27bc414
Andy Neillans <andy@neillans.co.uk>
2024-04-11 16:05:15 +0100

    This should be a working "Is Authenticated" call for Bluesky

--- PostyFox-TypeScript/BlueSky/isAuthenticated.ts
+++ PostyFox-TypeScript/BlueSky/isAuthenticated.ts
@@ -8,32 +8,51 @@ const { getUserId, validateAuth } = require("../Helpers/index");
 async function BlueSky_IsAuthenticated(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     if (validateAuth(request)) {
         const credential = new DefaultAzureCredential();
+        let body = (await request.body.getReader().read()).value;
 
-        // Service config for the user is held in ConfigTable
-        const configTableName = process.env["ConfigTable"];
-        if(!configTableName) throw new Error("ConfigTable is empty");
+        // Extract the serviceId from the POST JSON body
+        if (body) {
+            let jsonObject = JSON.parse(body);
+            
+            if (jsonObject.ServiceID) {
+                // Service config for the user is held in ConfigTable
+                const configTableName = process.env["ConfigTable"];
+                if(!configTableName) throw new Error("ConfigTable is empty");
 
-        const client = new TableClient(configTableName, "ConfigTable", credential);
-        await client.createTable(); // Ensure it exists
+                const client = new TableClient(configTableName, "ConfigTable", credential);
+                await client.createTable(); // Ensure it exists
 
-        const entity = await client.getEntity(getUserId(request), "BlueSky");
+                const entity = await client.getEntity(getUserId(request), jsonObject.ServiceID);
+                if (entity && entity.ServiceID == "BlueSky") {
+                    let configurationJson = JSON.parse(entity.Configuration.toString());
+                    // The BlueSky identity for a user is made up of a SecureConfiguration component which is stored in KeyVault (their App Password)
+                    // So we need to pull it now
+                    const keyVaultName = process.env["SecretStore"];
+                    if(!keyVaultName) throw new Error("SecretStore is empty");
 
-        // The BlueSky identity for a user is made up of a SecureConfiguration component which is stored in KeyVault (their App Password)
-        // So we need to pull it now
+                    const secretClient = new SecretClient(keyVaultName, credential);
+                    let secretName = jsonObject.ServiceID + "-" + getUserId(request);
+                    let secret = await secretClient.getSecret(secretName);
 
-        
-
-        const keyVaultName = process.env["SecretStore"];
-        if(!keyVaultName) throw new Error("SecretStore is empty");
-
+                    // Extract the password from the secret data, and try logging into the BSky platform. Return result.
+                    let secretJson = JSON.parse(secret.value);
+                    if (secretJson) {
+                        const agent = new BskyAgent({ service: 'https://bsky.social' });
+                        await agent.login({
+                            identifier: configurationJson.Handle,
+                            password: secretJson.AppPassword,
+                        }).then(res => { return res.success })
+                    }
+                }
+            }
+        }
+        return { status: 404 }
     }
 
-    return { body: "" };
-
-
+    return { body: "false" };
 }
 
 app.http('BlueSky_IsAuthenticated', {
-    methods: ['GET'],
+    methods: ['POST'],
     handler: BlueSky_IsAuthenticated
 });
