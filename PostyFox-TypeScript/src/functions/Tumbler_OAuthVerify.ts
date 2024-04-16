9f347acc443321a94bb56cbc705a3dd4eb08d06a
Andy Neillans <andy@neillans.co.uk>
2024-04-16 21:01:41 +0100

    OAuth for Tumblr

--- PostyFox-TypeScript/src/functions/Tumbler_OAuthVerify.ts
+++ PostyFox-TypeScript/src/functions/Tumbler_OAuthVerify.ts
@@ -15,13 +15,38 @@ async function Tumblr_OAuthVerify(request: HttpRequest, context: InvocationConte
         const secretClient = new SecretClient(keyVaultName, credential);
         let userId = getUserId(request);
         let secret = await secretClient.getSecret("TumblrState-" + userId); // Get the state from store
+        let secretClientId = await secretClient.getSecret("TumblrClientId");
+        let secretClientSecret = await secretClient.getSecret("TumblrClientSecret");
+        const tumblrRedirectUri = process.env["TumblrAuthRedirect"];
 
         if (secret != null && secret.value != null)
         {
             if (secret.value == request.query.get("state")) {
                 // State matches - woo!
                 // Continue with auth flow.
+                let accessTokenUri = "https://api.tumblr.com/v2/oauth2/token"; // POST op to this
                 
+                fetch(accessTokenUri, {
+                    method: 'POST',
+                    body: "grant_type=authorization_code&code=" + request.query.get("code") + "&client_id=" + secretClientId.value + "&client_secret=" + secretClientSecret.value + "&redirect_uri=" + tumblrRedirectUri,
+                    headers: {
+                        "Content-Type": "application/x-www-form-urlencoded",
+                    },
+                })
+                .then(response => response.json())
+                .then(async data => {
+                    // Remove the state from keyvault
+                    await secretClient.beginDeleteSecret("TumblrState-" + userId);
+                    // Save the access token - set the expiry time too
+                    let expiryTime = new Date(new Date().getTime() + data.expires_in * 1000);
+                    await secretClient.setSecret("TumblrAccessToken-" + userId, data.access_token, { expiresOn: expiryTime, tags: { ["Service"]: "Tumblr" }} );
+                    // Save the refresh token
+                    await secretClient.setSecret("TumblrRefreshToken-" + userId, data.refresh_token, { expiresOn: expiryTime, tags: { ["Service"]: "Tumblr" }} );
+
+                    return {
+                        status: 200
+                    }
+                });
             }
         }
 
@@ -29,7 +54,6 @@ async function Tumblr_OAuthVerify(request: HttpRequest, context: InvocationConte
             status: 404
         }
     }
-
 
     return {
         status: 403
@@ -53,6 +77,11 @@ async function Tumbler_GenerateAuthUrl(request: HttpRequest, context: Invocation
 
         let authUrl = "www.tumblr.com/oauth2/authorize?";
         authUrl += "client_id=" + secret.value + "&response_type=code&scope=basic%20write&state=" + state + "redirect_uri=" + tumblrRedirectUri
+
+        return {
+            status: 200,
+            body: "{\"AuthURL\":\"" + authUrl + "\"}"
+        }
     }
 
     return {
