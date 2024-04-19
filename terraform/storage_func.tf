cd1090a9066761855c5c59bcdf01653450031d98
Andy Neillans <andy@neillans.co.uk>
2024-04-19 09:15:19 +0100

    Had missed a storage account

--- terraform/storage_func.tf
+++ terraform/storage_func.tf
@@ -4,7 +4,7 @@ resource "azurerm_storage_account" "linux_func_storage" {
   location                        = azurerm_resource_group.rg.location
   account_tier                    = "Standard"
   account_replication_type        = "LRS"
-  allow_nested_items_to_be_public = false
+  allow_nested_items_to_be_public = true
 
   infrastructure_encryption_enabled = true
 
@@ -44,4 +44,37 @@ resource "azurerm_storage_account" "linux_funcnet_storage" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+
+  allow_nested_items_to_be_public = true
+
+  infrastructure_encryption_enabled = true
+
+  shared_access_key_enabled = false
+
+  public_network_access_enabled = true
+
+  network_rules {
+    bypass         = ["Logging", "Metrics", "AzureServices"]
+    default_action = "Deny"
+
+    ip_rules = var.allowed_ips
+  }
+
+  blob_properties {
+    delete_retention_policy {
+      days = 7
+    }
+
+    versioning_enabled = true
+
+    container_delete_retention_policy {
+      days = 7
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [ 
+      network_rules[0].ip_rules
+     ]
+  }
 }
