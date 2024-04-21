efefaea3dddaf3dfa9ee12993981cc2ecfe3701d
Andy Neillans <andy@neillans.co.uk>
2024-04-21 21:42:24 +0100

    Adding Profile capability to generate an API key

---
+++ PostyFox-NetCore/ProfileAPIKeyTableEntity.cs
@@ -0,0 +1,19 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System.ComponentModel;
+
+namespace PostyFox_NetCore
+{
+#pragma warning disable CS8618
+    public class ProfileAPIKeyTableEntity : ITableEntity
+    {
+        [Description("UserID")]
+        public string PartitionKey { get; set; }
+        [Description("Unique API Key ID")]
+        public string RowKey { get; set; }
+        public string APIKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+#pragma warning restore CS8618
+}
