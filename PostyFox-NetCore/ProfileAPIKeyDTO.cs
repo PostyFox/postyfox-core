7668dbd77a32dd7ce8e4de56c1f2d71a9b04b58a
Andy Neillans <andy@neillans.co.uk>
2024-04-22 09:25:53 +0100

    50 3rdparty api (#54) (Part 1)
    * Adding Profile capability to generate an API key
    
    * Bump Microsoft.Extensions.Azure and dependencies

---
+++ PostyFox-NetCore/ProfileAPIKeyDTO.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+
+namespace PostyFox_NetCore
+{
+    public class ProfileAPIKeyDTO
+    {
+        [OpenApiPropertyAttribute(Description = "The internal unique identifier for this API Key")]
+        public string? ID { get; set; }
+
+        [OpenApiPropertyAttribute(Description = "API Key")]
+        public string? APIKey { get; set; }
+
+        [OpenApiPropertyAttribute(Description = "User ID")]
+        public string? UserID { get; set; }
+    }
+}
