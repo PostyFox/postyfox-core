721f33be65100009e01751df2f389fabdc263f12
Andy Neillans <andy@neillans.co.uk>
2024-05-01 11:45:52 +0100

    Set the azure client id on fa's

--- terraform/nodejs_function_app.tf
+++ terraform/nodejs_function_app.tf
@@ -16,6 +16,7 @@ resource "azurerm_linux_function_app" "nodejs_func_app" {
     "SecretStore"                            = azurerm_key_vault.key_vault.vault_uri
     "StorageAccount"                         = azurerm_storage_account.data_storage.primary_blob_endpoint
     "AAD_B2C_PROVIDER_AUTHENTICATION_SECRET" = "@Microsoft.KeyVault(VaultName=${local.appname}-kv${local.hyphen-env};SecretName=clientsecret)"
+    "AZURE_CLIENT_ID"                        = azurerm_user_assigned_identity.func_apps_uai.client_id    
   }
 
   identity {
--- terraform/post_function_app.tf
+++ terraform/post_function_app.tf
@@ -20,6 +20,7 @@ resource "azurerm_linux_function_app" "dotnet_funcpost_app" {
     "SecretStore"                            = azurerm_key_vault.key_vault.vault_uri
     "StorageAccount"                         = azurerm_storage_account.data_storage.primary_blob_endpoint
     "AAD_B2C_PROVIDER_AUTHENTICATION_SECRET" = "@Microsoft.KeyVault(VaultName=${local.appname}-kv${local.hyphen-env};SecretName=clientsecret)"
+    "AZURE_CLIENT_ID"                        = azurerm_user_assigned_identity.func_apps_uai.client_id    
   }
 
   site_config {
