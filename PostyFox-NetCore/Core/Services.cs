948415002886fb274d4b4c6784cceaa6563b323d
Andy Neillans <andy@neillans.co.uk>
2024-05-01 20:07:07 +0100

    Correct IsEnabled reference

--- PostyFox-NetCore/Core/Services.cs
+++ PostyFox-NetCore/Core/Services.cs
@@ -160,7 +160,7 @@ namespace PostyFox_NetCore
                         ServiceName = data.ServiceName,
                         Configuration = data.Configuration,
                         Timestamp = DateTime.UtcNow,
-                        IsEnabled = data.Enabled,
+                        IsEnabled = data.IsEnabled,
                         RowKey = data.ID
                     };
                     client.UpsertEntity(tableEntity);
