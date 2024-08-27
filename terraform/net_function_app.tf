1930dfec82aaedfda63251c81944dfb0408aba81
Andy Neillans <andy@neilans.co.uk>
2024-08-27 17:06:47 +0100

    Permissions shuffle

--- terraform/net_function_app.tf
+++ terraform/net_function_app.tf
@@ -122,21 +122,51 @@ resource "azurerm_monitor_diagnostic_setting" "dotnet_func_app" {
   }
 }
 
-// Add application identity to Storage Blob Reader on storage account
-resource "azurerm_role_assignment" "dotnetfuncapp-data" {
+// Permissions ...
+resource "azurerm_role_assignment" "dotnetuai-data" {
   scope                = azurerm_storage_account.linux_funcnet_storage.id
-  role_definition_name = "Storage Blob Data Reader"
+  role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_user_assigned_identity.func_apps_uai.principal_id
 }
 
-resource "azurerm_role_assignment" "dotnetfuncapp-data-posting" {
+resource "azurerm_role_assignment" "dotnetuai-table" {
+  scope                = azurerm_storage_account.linux_funcnet_storage.id
+  role_definition_name = "Storage Blob Table Contributor"
+  principal_id         = azurerm_user_assigned_identity.func_apps_uai.principal_id
+}
+
+resource "azurerm_role_assignment" "dotnetuai-data-posting" {
   scope                = azurerm_storage_account.linux_funcpost_storage.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_user_assigned_identity.func_apps_uai.principal_id
 }
 
-resource "azurerm_role_assignment" "dotnetfuncapp-queuecontributor" {
+resource "azurerm_role_assignment" "dotnetuai-queue-posting" {
   scope                = azurerm_storage_account.linux_funcpost_storage.id
   role_definition_name = "Storage Queue Data Contributor"
   principal_id         = azurerm_user_assigned_identity.func_apps_uai.principal_id
+}
+
+resource "azurerm_role_assignment" "dotnetfuncapp-data" {
+  scope                = azurerm_storage_account.linux_funcnet_storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_linux_function_app.dotnet_func_app.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "dotnetfuncapp-table" {
+  scope                = azurerm_storage_account.linux_funcnet_storage.id
+  role_definition_name = "Storage Blob Table Contributor"
+  principal_id         = azurerm_linux_function_app.dotnet_func_app.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "dotnetfuncapp-data-posting" {
+  scope                = azurerm_storage_account.linux_funcpost_storage.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_linux_function_app.dotnet_func_app.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "dotnetfuncapp-queue-posting" {
+  scope                = azurerm_storage_account.linux_funcpost_storage.id
+  role_definition_name = "Storage Queue Data Contributor"
+  principal_id         = azurerm_linux_function_app.dotnet_func_app.identity[0].principal_id
 }
--- terraform/resource_group.tf
+++ terraform/resource_group.tf
@@ -2,39 +2,3 @@ resource "azurerm_resource_group" "rg" {
   name     = "${local.appname}-rg${local.hyphen-env}"
   location = local.location
 }
-
-resource "azurerm_role_assignment" "sp-datacontributor" {
-  scope                = azurerm_resource_group.rg.id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_user_assigned_identity.func_apps_uai.principal_id
-}
-
-resource "azurerm_role_assignment" "sp-tablecontributor" {
-  scope                = azurerm_resource_group.rg.id
-  role_definition_name = "Storage Table Data Contributor"
-  principal_id         = azurerm_user_assigned_identity.func_apps_uai.principal_id
-}
-
-resource "azurerm_role_assignment" "sp-queuecontributor" {
-  scope                = azurerm_resource_group.rg.id
-  role_definition_name = "Storage Queue Data Contributor"
-  principal_id         = azurerm_user_assigned_identity.func_apps_uai.principal_id
-}
-
-resource "azurerm_role_assignment" "sp-datacontributor-net" {
-  scope                = azurerm_resource_group.rg.id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azurerm_linux_function_app.dotnet_func_app.identity[0].principal_id
-}
-
-resource "azurerm_role_assignment" "sp-tablecontributor-net" {
-  scope                = azurerm_resource_group.rg.id
-  role_definition_name = "Storage Table Data Contributor"
-  principal_id         = azurerm_linux_function_app.dotnet_func_app.identity[0].principal_id
-}
-
-resource "azurerm_role_assignment" "sp-queuecontributor-net" {
-  scope                = azurerm_resource_group.rg.id
-  role_definition_name = "Storage Queue Data Contributor"
-  principal_id         = azurerm_linux_function_app.dotnet_func_app.identity[0].principal_id
-}
