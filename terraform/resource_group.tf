cbe9051658ad6f738ad2b140ad600f8f8cc038ef
Andy Neillans <andy@neilans.co.uk>
2024-08-27 15:28:49 +0100

    Monster mash perms

--- terraform/resource_group.tf
+++ terraform/resource_group.tf
@@ -20,3 +20,21 @@ resource "azurerm_role_assignment" "sp-queuecontributor" {
   role_definition_name = "Storage Queue Data Contributor"
   principal_id         = azurerm_user_assigned_identity.func_apps_uai.principal_id
 }
+
+resource "azurerm_role_assignment" "sp-datacontributor-net" {
+  scope                = azurerm_resource_group.rg.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = azurerm_linux_function_app.dotnet_func_app.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "sp-tablecontributor-net" {
+  scope                = azurerm_resource_group.rg.id
+  role_definition_name = "Storage Table Data Contributor"
+  principal_id         = azurerm_linux_function_app.dotnet_func_app.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "sp-queuecontributor-net" {
+  scope                = azurerm_resource_group.rg.id
+  role_definition_name = "Storage Queue Data Contributor"
+  principal_id         = azurerm_linux_function_app.dotnet_func_app.identity[0].principal_id
+}
