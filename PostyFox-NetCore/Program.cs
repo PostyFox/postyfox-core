78406a6afaab028b02b85f34c7cedd992d231152
Andy Neillans <andy@neillans.co.uk>
2024-09-30 19:13:18 +0100

    More logging

--- PostyFox-NetCore/Program.cs
+++ PostyFox-NetCore/Program.cs
@@ -7,16 +7,19 @@ using Azure.Security.KeyVault.Secrets;
 using Twitch.Net.EventSub;
 using Azure.Core;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
-var tableAccount = Environment.GetEnvironmentVariable("ConfigTable") ?? throw new Exception("Configuration not found for ConfigTable");
-var storageAccount = Environment.GetEnvironmentVariable("StorageAccount") ?? throw new Exception("Configuration not found for StorageAccount");
+// Load the configuration from the environment variables
+var tableAccount = Environment.GetEnvironmentVariable("ConfigTable");
+var storageAccount = Environment.GetEnvironmentVariable("StorageAccount");
 
-var twitchClientId = Environment.GetEnvironmentVariable("TwitchClientId") ?? throw new Exception("Configuration not found for TwitchClientId");
-var twitchCallbackUrl = Environment.GetEnvironmentVariable("TwitchCallbackUrl") ?? throw new Exception("Configuration not found for TwitchCallbackUrl");
+var twitchClientId = Environment.GetEnvironmentVariable("TwitchClientId");
+var twitchCallbackUrl = Environment.GetEnvironmentVariable("TwitchCallbackUrl");
 
 var twitchClientSecret = "";
 var twitchSignatureSecret = "";
 
+// Create the host
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication(worker => worker.UseNewtonsoftJson())
     .ConfigureServices(services =>
@@ -25,8 +28,16 @@ var host = new HostBuilder()
         {
             // Register clients for each service
 #pragma warning disable CS8604
-            clientBuilder.AddTableServiceClient(new Uri(tableAccount)).WithName("ConfigTable");
-            clientBuilder.AddBlobServiceClient(new Uri(storageAccount)).WithName("StorageAccount");
+            if (!string.IsNullOrEmpty(tableAccount))
+            {
+                clientBuilder.AddTableServiceClient(new Uri(tableAccount)).WithName("ConfigTable");
+            }
+
+            if (!string.IsNullOrEmpty(storageAccount))
+            {
+                clientBuilder.AddBlobServiceClient(new Uri(storageAccount)).WithName("StorageAccount");
+            }
+
             if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("SecretStore")))
             {
                 clientBuilder.AddSecretClient(new Uri(Environment.GetEnvironmentVariable("SecretStore"))).WithName("SecretStore");
@@ -44,19 +55,55 @@ var host = new HostBuilder()
             twitchSignatureSecret = _secretStore.GetSecret("TwitchSignatureSecret").Value.ToString(); ;
         }
 
-        services.AddTwitchApiClient(config =>
+        if (!string.IsNullOrEmpty(twitchClientId) && !string.IsNullOrEmpty(twitchClientSecret))
         {
-            config.ClientId = twitchClientId;
-            config.ClientSecret = twitchClientSecret;
-        });
+            services.AddTwitchApiClient(config =>
+            {
+                config.ClientId = twitchClientId;
+                config.ClientSecret = twitchClientSecret;
+            });
+        }
 
-        var eventSubConfig = new EventSubConfig();
-        eventSubConfig.SignatureSecret = twitchSignatureSecret;
-        eventSubConfig.ClientId = twitchClientId;
-        eventSubConfig.ClientSecret = twitchClientSecret;
-        eventSubConfig.CallbackUrl = twitchCallbackUrl;
-        services.AddTwitchEventSubService(eventSubConfig);
+        if (!string.IsNullOrEmpty(twitchClientId) && !string.IsNullOrEmpty(twitchClientSecret) && 
+            !string.IsNullOrEmpty(twitchCallbackUrl) && !string.IsNullOrEmpty(twitchSignatureSecret))
+        {
+            services.AddTwitchEventSubService(config =>
+            {
+                config.SignatureSecret = twitchSignatureSecret;
+                config.ClientId = twitchClientId;
+                config.ClientSecret = twitchClientSecret;
+                config.CallbackUrl = twitchCallbackUrl;
+            });
+        }       
+    })
+    .ConfigureLogging(logging =>
+    {
+        logging.AddConsole();
     })
     .Build();
+
+ILogger logger = host.Services.GetService<ILogger<Program>>();
+
+logger.LogInformation("Starting PostyFox-NetCore");
+logger.LogInformation("Configuration loaded");
+
+logger.LogInformation("Table Account: {tableAccount}", tableAccount);
+logger.LogInformation("Storage Account: {storageAccount}", storageAccount);
+logger.LogInformation("Twitch Client ID: {twitchClientId}", twitchClientId);
+logger.LogInformation("Twitch Callback URL: {twitchCallbackUrl}", twitchCallbackUrl);
+if (string.IsNullOrEmpty(twitchClientSecret))
+{
+    logger.LogInformation("Twitch Client Secret: NOT DEFINED");
+}
+if (string.IsNullOrEmpty(twitchSignatureSecret))
+{
+    logger.LogInformation("Twitch Signature Secret: NOT DEFINED");
+}
+if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("SecretStore"))) {
+    logger.LogInformation("Secret Store: NOT DEFINED");
+} else
+{
+    logger.LogInformation("Secret Store: {secretStore}", Environment.GetEnvironmentVariable("SecretStore"));
+}
 
 host.Run();
