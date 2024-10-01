acca55c7a727a8eb40ff11b97f3df889f426d9f3
Andy Neillans <andy@neillans.co.uk>
2024-10-01 09:22:52 +0100

    Updating back from Basic ASP

--- terraform/function_app_service_plan.tf
+++ terraform/function_app_service_plan.tf
@@ -1,11 +1,13 @@
-resource "azurerm_service_plan" "linux_func_service_plan" {
-  name                = "${local.appname}-asp${local.hyphen-env}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  os_type             = "Linux"
-  # sku_name            = "Y1" # Consumption Plan
-  sku_name            = "B1"
-}
+// This shouldn't be needed ... for now at least
+
+# resource "azurerm_service_plan" "linux_func_service_plan" {
+#   name                = "${local.appname}-asp${local.hyphen-env}"
+#   resource_group_name = azurerm_resource_group.rg.name
+#   location            = azurerm_resource_group.rg.location
+#   os_type             = "Linux"
+#   # sku_name            = "Y1" # Consumption Plan
+#   sku_name            = "B1"
+# }
 
 resource "azurerm_service_plan" "linux_consumption_func_service_plan" {
   name                = "${local.appname}-consum_asp${local.hyphen-env}"
--- terraform/net_function_app.tf
+++ terraform/net_function_app.tf
@@ -8,7 +8,7 @@ resource "azurerm_linux_function_app" "dotnet_func_app" {
 
   storage_account_name          = azurerm_storage_account.linux_funcnet_storage.name
   storage_uses_managed_identity = true
-  service_plan_id               = azurerm_service_plan.linux_func_service_plan.id
+  service_plan_id               = azurerm_service_plan.linux_consumption_func_service_plan.id
 
   identity {
     type = "SystemAssigned"
