bc64917d050ef5141d67f76c75bfe47e487588c2
Andy Neillans <andy@neillans.co.uk>
2024-10-08 22:41:04 +0100

    Remove unused app service plans

--- terraform/function_app_service_plan.tf
+++
@@ -1,9 +0,0 @@
-// This shouldn't be needed ... for now at least
-
-resource "azurerm_service_plan" "linux_consumption_func_service_plan" {
-  name                = "${local.appname}-consum_asp${local.hyphen-env}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  os_type             = "Linux"
-  sku_name            = "Y1" # Consumption Plan
-}
