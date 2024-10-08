a2479405c5526471a79abb0a4daf39f2c6ac2b92
Andy Neillans <andy@neilans.co.uk>
2024-10-08 16:40:37 +0100

    Missed azapi, removing the version pin in azurerm

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,9 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.3.0"
+    }
+    azapi = {
+      source = "Azure/azapi"
     }
   }
 
