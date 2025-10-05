536bb58941713af6d8f4960471db015c9d7a5563
Andy Neillans <andy@neillans.co.uk>
2025-10-05 20:08:14 +0100

    fixes

--- terraform/entra_external.tf
+++ terraform/entra_external.tf
@@ -3,7 +3,7 @@ module "extra_external_id" {
 
   resource_group_id = azurerm_resource_group.rg.id
 
-  domain_name = "postyfox.onmicrosoft.com"
+  domain_name = var.entra_tenant_id
 
   display_name      = "PostyFox"
   country_code      = "GB"
