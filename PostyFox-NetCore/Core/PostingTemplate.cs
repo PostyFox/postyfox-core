2db398abcbf3abbdc0611d999301eae0d4d0b3af
Andy Neillans <andy@neillans.co.uk>
2025-12-06 21:08:15 +0000

    * 'Line 13: Added the using directive for Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums to bring OpenApiVisibilityType into scope and resolve CS0103.' in file 'PostyFox-NetCore\Core\PostingTemplate.cs'

--- PostyFox-NetCore/Core/PostingTemplate.cs
+++ PostyFox-NetCore/Core/PostingTemplate.cs
@@ -11,6 +11,7 @@ using System.Net;
 using static PostyFox_NetCore.Integrations.Telegram;
 using static PostyFox_NetCore.Services;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 
 namespace PostyFox_NetCore
 {
